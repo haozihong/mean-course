621c6a6833cf933b6a2f2cd20d42ab6d6c0f397e
haozihong <haozihong@hotmail.com>
2020-06-09 21:04:08 -0400

    create a chain of adding post, saving, and display

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -6,8 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = 'NO CONTENT';
+  enteredTitle = '';
+  enteredContent = '';
+  @Output() postCreated = new EventEmitter();
 
   // convention to start methord for event with a 'on'
   // onAddPost(postInput: HTMLTextAreaElement) {
@@ -16,6 +17,10 @@ export class PostCreateComponent {
   // }
 
   onAddPost() {
-    this.newPost = this.enteredValue;
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+    this.postCreated.emit(post);
   }
 }
