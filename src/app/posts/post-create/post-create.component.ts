7cafd772f981a497cfb40e968fac22cfb3ee7723
haozihong <haozihong@hotmail.com>
2020-06-10 00:33:00 -0400

    implement adding post using injection, oberserve (rxjs)

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
-import { Post } from '../post.model'
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -11,7 +11,8 @@ import { Post } from '../post.model'
 export class PostCreateComponent {
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated = new EventEmitter<Post>();
+
+  constructor(public postsService: PostsService) {}
 
   // convention to start methord for event with a 'on'
   // onAddPost(postInput: HTMLTextAreaElement) {
@@ -21,10 +22,6 @@ export class PostCreateComponent {
 
   onAddPost(form: NgForm) {
     if (form.invalid) return;
-    const post: Post = {
-      title: form.value.title,
-      content: form.value.content
-    };
-    this.postCreated.emit(post);
+    this.postsService.addPost(form.value.title, form.value.content);
   }
 }
