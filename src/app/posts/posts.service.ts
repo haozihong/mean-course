ab1933d57bf0d3c449b49380ec296aa6c4e66604
haozihong <haozihong@hotmail.com>
2020-06-10 20:55:19 -0400

    add form field titles; implement form self-clear after submission; add 2 buttons on each post (appearance only)

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -3,9 +3,13 @@ import { Subject } from 'rxjs';
 
 import { Post } from './post.model';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PostsService {
-  private posts: Post[] = [];
+  private posts: Post[] = [
+    { title: 'First Post', content: "This is the first post's content" },
+    { title: 'Second Post', content: "This is the second post's content" },
+    { title: 'Third Post', content: "This is the third post's content" },
+  ];
   private postsUpdated = new Subject<Post[]>();
 
   getPosts() {
@@ -17,7 +21,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = {title: title, content: content};
+    const post: Post = { title: title, content: content };
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
