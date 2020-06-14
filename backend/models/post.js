0fc90d9771bfec43abbc068a13053cbbd001dae9
haozihong <haozihong@hotmail.com>
2020-06-13 22:16:53 -0400

    install mongoose and add a schema for post

---
+++ backend/models/post.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+// schema is a blueprint
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Posts", postSchema);
