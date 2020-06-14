1d4ff9f691ba7799ad613c0b6ad07623e7acddfb
haozihong <haozihong@hotmail.com>
2020-06-14 02:37:49 -0400

    send db data from back-end to front-end

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,25 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
-  private posts: Post[] = [
-    { id: null, title: 'First Post', content: "This is the first post's content" },
-    { id: null, title: 'Second Post', content: "This is the second post's content" },
-    { id: null, title: 'Third Post', content: "This is the third post's content" },
-  ];
+  private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        console.log(postData.message);
-        this.posts = postData.posts;
+    this.http
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postData) =>
+          postData.posts.map((post) => ({
+            title: post.title,
+            content: post.content,
+            id: post._id,
+          }))
+        )
+      )
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
     return [...this.posts];
@@ -31,7 +37,8 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
-    this.http.post<{ message: string }>("http://localhost:3000/api/posts", post)
+    this.http
+      .post<{ message: string }>('http://localhost:3000/api/posts', post)
       .subscribe((responseData) => {
         console.log(responseData.message);
         this.posts.push(post);
