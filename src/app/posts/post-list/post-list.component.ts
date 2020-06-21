11f53f814a35cccc918b3876dbc795b0cdc96131
haozihong <haozihong@hotmail.com>
2020-06-21 18:01:54 -0400

    implement and initial pagination (fetching each page from backend)

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model'
@@ -12,6 +13,10 @@ import { PostsService } from '../posts.service';
 export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   isLoading = false;
+  totalPosts = 10;
+  postsPerPage = 2;
+  currentPage = 1;
+  pageSizeOptions = [1, 2, 5, 10];
   private postsSub: Subscription;
 
   // like declare a property and store the incoming value in it
@@ -23,12 +28,19 @@ export class PostListComponent implements OnInit, OnDestroy {
       .subscribe((posts: Post[]) => {
         this.isLoading = false;
         this.posts = posts;
+        // this.totalPosts = posts.length;
       });
-    this.postsService.getPosts();
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
   }
 
   ngOnDestroy() {
     this.postsSub.unsubscribe();
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    this.postsPerPage = pageData.pageSize;
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
   }
 
   onDelete(postId: string) {
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -13,9 +13,10 @@ export class PostsService {
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  getPosts() {
+  getPosts(postPerPage: number, currentPage: number) {
+    const queryPatams = `?pagesize=${postPerPage}&page=${currentPage}`;
     this.http
-      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts' + queryPatams)
       .pipe(
         map((postData) =>
           postData.posts.map((post) => ({
