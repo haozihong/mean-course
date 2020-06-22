233ee05455c5ebf0e3574663162ebe9d2c69e89c
haozihong <haozihong@hotmail.com>
2020-06-21 22:51:06 -0400

    bind Template to form and add validators

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   isLoading = false;
+  form: NgForm;
+
+  onLogin(loginForm: NgForm) {
+    console.log(loginForm.value);
+  }
 }
