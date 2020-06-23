2a17e1f0cd14559cbc2d96c4cf547798e2fe8ce2
haozihong <haozihong@hotmail.com>
2020-06-23 01:09:27 -0400

    add a check-auth middleware and use it in posts routes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const router = express.Router();
 
 const Post = require("../models/post");
+const checkAuth = require("../middleware/check-auth");
 
 const MIME_TYPE_MAP = {
   "image/png": "png",
@@ -23,41 +24,51 @@ const storage = multer.diskStorage({
   },
 });
 
-router.post("/", multer({ storage: storage }).single("image"), (req, res) => {
-  const url = req.protocol + "://" + req.get("host");
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: url + "/images/" + req.file.filename,
-  });
-  post.save().then((createdPost) => {
-    res.status(201).json({
-      message: "Post added successfully.",
-      post: {
-        id: createdPost._id,
-        title: createdPost.title,
-        content: createdPost.content,
-        imagePath: createdPost.imagePath,
-      }
+router.post(
+  "/",
+  checkAuth,
+  multer({ storage: storage }).single("image"),
+  (req, res) => {
+    const url = req.protocol + "://" + req.get("host");
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: url + "/images/" + req.file.filename,
     });
-  });
-});
+    post.save().then((createdPost) => {
+      res.status(201).json({
+        message: "Post added successfully.",
+        post: {
+          id: createdPost._id,
+          title: createdPost.title,
+          content: createdPost.content,
+          imagePath: createdPost.imagePath,
+        },
+      });
+    });
+  }
+);
 
-router.put("/:id", multer({ storage: storage }).single("image"), (req, res) => {
-  let imagePath = req.file
-    ? req.protocol + "://" + req.get("host") + "/images/" + req.file.filename
-    : req.body.imagePath;
-  const post = new Post({
-    _id: req.params.id,
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: imagePath,
-  });
-  console.log(post.toObject());
-  Post.updateOne({ _id: req.params.id }, post).then((result) => {
-    res.status(200).json({ message: "Update successful!" });
-  });
-});
+router.put(
+  "/:id",
+  checkAuth,
+  multer({ storage: storage }).single("image"),
+  (req, res) => {
+    let imagePath = req.file
+      ? req.protocol + "://" + req.get("host") + "/images/" + req.file.filename
+      : req.body.imagePath;
+    const post = new Post({
+      _id: req.params.id,
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: imagePath,
+    });
+    console.log(post.toObject());
+    Post.updateOne({ _id: req.params.id }, post).then((result) => {
+      res.status(200).json({ message: "Update successful!" });
+    });
+  }
+);
 
 router.get("/", (req, res) => {
   const pageSize = Number(req.query.pagesize);
@@ -92,7 +103,7 @@ router.get("/:id", (req, res) => {
   });
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then((result) => {
     console.log(result);
     res.status(200).json({ message: "Post deleted!" });
