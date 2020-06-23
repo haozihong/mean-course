1b187f061ac477b14a780a6e0b15b8677e99102f
haozihong <haozihong@hotmail.com>
2020-06-23 17:11:51 -0400

    implement header button change accoring to auth status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 import { AuthData } from './auth-data.model';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService{
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient) {}
 
   getToken() {
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string) {
@@ -26,6 +32,11 @@ export class AuthService{
     this.http.post<{ token: string }>('http://localhost:3000/api/user/login', authData)
       .subscribe(response => {
         this.token = response.token;
+        this.authStatusListener.next(true);
       })
+  }
+
+  logout() {
+
   }
 }
