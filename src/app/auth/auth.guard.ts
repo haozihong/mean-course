36721e8141d47461c4eb84b18a7bfb0ab6fad0b5
haozihong <haozihong@hotmail.com>
2020-06-23 23:49:56 -0400

    add frontend route guard to 2 routes

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {};
+
+  // if return true, the router know this route is accessable
+  // if return false, should provide an alternative, otherwise just blocking the route
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
+     const isAuth = this.authService.getIsAuth();
+     if (!isAuth) {
+      this.router.navigate(['/login']);
+     }
+     return isAuth;
+  }
+}
